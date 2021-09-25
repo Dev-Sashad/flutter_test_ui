e17b291285af97d480d168e210952b4ed5bfd710
Sanni Shafeeq <62085280+sashad97@users.noreply.github.com>
2021-09-25 08:11:41 +0100

    new

--- lib/app/auth_screen/sign_up/signup_view.dart
+++ lib/app/auth_screen/sign_up/signup_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test_ui/app/auth_screen/sign_up/signup_viewmodel.dart';
 import 'package:flutter_test_ui/constants/colors.dart';
 import 'package:flutter_test_ui/constants/size_config.dart';
@@ -29,161 +30,168 @@ class _SignUpViewState extends State<SignUpView> {
   Widget build(BuildContext context) {
     return ViewModelBuilder<SignUpViewModel>.reactive(
       viewModelBuilder: () => SignUpViewModel(),
-      builder: (context, model, child) => GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
+      builder: (context, model, child) => WillPopScope(
+        onWillPop: () {
+          SystemNavigator.pop();
+          return null;
         },
-        child: SafeArea(
-          child: Scaffold(
-              resizeToAvoidBottomInset: true,
-              backgroundColor: AppColors.white,
-              body: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                        height: kheight(15, context),
-                        width: kwidth(50, context),
-                        child: Image.asset(
-                          'assets/images/top_bck.png',
-                          fit: BoxFit.fill,
-                        )),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: kheight(18, context)),
-                    alignment: Alignment.center,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('Welcome Onboard',
-                            style: TextStyle(
-                                fontSize: 13,
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: SafeArea(
+            child: Scaffold(
+                resizeToAvoidBottomInset: true,
+                backgroundColor: AppColors.white,
+                body: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                          height: kheight(15, context),
+                          width: kwidth(50, context),
+                          child: Image.asset(
+                            'assets/images/top_bck.png',
+                            fit: BoxFit.fill,
+                          )),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: kheight(18, context)),
+                      alignment: Alignment.center,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Welcome Onboard',
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  letterSpacing: 5,
+                                  color: AppColors.primaryColor,
+                                  fontWeight: FontWeight.bold)),
+                          customYMargin(kheight(3, context)),
+                          Text('Vulputate vitate enim.Vulputate',
+                              style: TextStyle(
+                                fontSize: 11,
                                 letterSpacing: 5,
-                                color: AppColors.primaryColor,
-                                fontWeight: FontWeight.bold)),
-                        customYMargin(kheight(3, context)),
-                        Text('Vulputate vitate enim.Vulputate',
-                            style: TextStyle(
-                              fontSize: 11,
-                              letterSpacing: 5,
-                              color: Colors.green[300],
-                            )),
-                        customYMargin(kheight(1, context)),
-                        Text('vitate enim',
-                            style: TextStyle(
-                              fontSize: 11,
-                              letterSpacing: 5,
-                              color: Colors.green[300],
-                            )),
-                      ],
+                                color: Colors.green[300],
+                              )),
+                          customYMargin(kheight(1, context)),
+                          Text('vitate enim',
+                              style: TextStyle(
+                                fontSize: 11,
+                                letterSpacing: 5,
+                                color: Colors.green[300],
+                              )),
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: kheight(35, context),
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: kwidth(10, context),
-                    ),
-                    child: ListView(
-                      physics: BouncingScrollPhysics(),
-                      children: [
-                        Container(
-                          child: Form(
-                            key: _key,
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            onChanged: () => setState(() =>
-                                _isValidated = _key.currentState.validate()),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                customYMargin(2),
-                                CustomTextFormField(
-                                  label: "Name",
-                                  textInputType: TextInputType.name,
-                                  controller: nameController,
-                                  validator: nameVal,
-                                  onChanged: (input) => model.setName(input),
-                                ),
-                                CustomTextFormField(
-                                  label: "Email",
-                                  textInputType: TextInputType.emailAddress,
-                                  controller: emailController,
-                                  validator: emailVal,
-                                  onChanged: (input) => model.setEmail(input),
-                                ),
-                                CustomTextFormField(
-                                  label: "Username",
-                                  textInputType: TextInputType.name,
-                                  controller: userNameController,
-                                  validator: usernameVal,
-                                  onChanged: (input) =>
-                                      model.setUserName(input),
-                                ),
-                                CustomTextFormField(
-                                  label: "Password",
-                                  textInputType: TextInputType.visiblePassword,
-                                  obscured: visiblePassword,
-                                  controller: passwordController,
-                                  validator: passwordVal,
-                                  onChanged: (input) =>
-                                      model.setPassword(input),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        customYMargin(kwidth(10, context)),
-                        CustomButton(
-                          title: 'Register',
-                          height: 7,
-                          width: kwidth(80, context),
-                          margin: 1,
-                          online: true,
-                          onPressed: () async {
-                            if (_isValidated == true) {
-                              await model.onSignUp(context);
-                              FocusScope.of(context).unfocus();
-                              // nameController.clear();
-                              // emailController.clear();
-                              // userNameController.clear();
-                              // passwordController.clear();
-                            } else {
-                              showToast('fields can\'t be empty');
-                            }
-                          },
-                        ),
-                        customYMargin(kwidth(5, context)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Already have an account?',
-                              style:
-                                  TextStyle(color: Colors.grey, fontSize: 12),
-                            ),
-                            customXMargin(3),
-                            InkWell(
-                              onTap: () {
-                                model.navigateToSignIn();
-                              },
-                              child: Text(
-                                'Sign in',
-                                style: TextStyle(
-                                    color: Colors.green,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w700),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: kheight(35, context),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: kwidth(10, context),
+                      ),
+                      child: ListView(
+                        physics: BouncingScrollPhysics(),
+                        children: [
+                          Container(
+                            child: Form(
+                              key: _key,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              onChanged: () => setState(() =>
+                                  _isValidated = _key.currentState.validate()),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  customYMargin(2),
+                                  CustomTextFormField(
+                                    label: "Name",
+                                    textInputType: TextInputType.name,
+                                    controller: nameController,
+                                    validator: nameVal,
+                                    onChanged: (input) => model.setName(input),
+                                  ),
+                                  CustomTextFormField(
+                                    label: "Email",
+                                    textInputType: TextInputType.emailAddress,
+                                    controller: emailController,
+                                    validator: emailVal,
+                                    onChanged: (input) => model.setEmail(input),
+                                  ),
+                                  CustomTextFormField(
+                                    label: "Username",
+                                    textInputType: TextInputType.name,
+                                    controller: userNameController,
+                                    validator: usernameVal,
+                                    onChanged: (input) =>
+                                        model.setUserName(input),
+                                  ),
+                                  CustomTextFormField(
+                                    label: "Password",
+                                    textInputType:
+                                        TextInputType.visiblePassword,
+                                    obscured: visiblePassword,
+                                    controller: passwordController,
+                                    validator: passwordVal,
+                                    onChanged: (input) =>
+                                        model.setPassword(input),
+                                  ),
+                                ],
                               ),
                             ),
-                          ],
-                        )
-                      ],
+                          ),
+                          customYMargin(kwidth(10, context)),
+                          CustomButton(
+                            title: 'Register',
+                            height: 7,
+                            width: kwidth(80, context),
+                            margin: 1,
+                            online: true,
+                            onPressed: () async {
+                              if (_isValidated == true) {
+                                await model.onSignUp(context);
+                                FocusScope.of(context).unfocus();
+                                // nameController.clear();
+                                // emailController.clear();
+                                // userNameController.clear();
+                                // passwordController.clear();
+                              } else {
+                                showToast('fields can\'t be empty');
+                              }
+                            },
+                          ),
+                          customYMargin(kwidth(5, context)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Already have an account?',
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 12),
+                              ),
+                              customXMargin(3),
+                              InkWell(
+                                onTap: () {
+                                  model.navigateToSignIn();
+                                },
+                                child: Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              )),
+                  ],
+                )),
+          ),
         ),
       ),
     );
